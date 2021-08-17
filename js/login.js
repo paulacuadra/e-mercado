d99457298f9cb8d4775d3304c3a0336c4f49eff7
paulacuadra <88450878+paulacuadra@users.noreply.github.com>
2021-08-17 15:11:09 -0300

    login

--- js/login.js
+++ js/login.js
@@ -2,5 +2,27 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("submitBtn").addEventListener('click', function () {
+        let inputUser = document.getElementById("inputUser");
+        let inputPass = document.getElementById("inputPass");
+        let camposCompletos = true;
 
+        if(inputUser.value==='') {
+            camposCompletos = false;
+            inputUser.classList.add("invalid");
+        }else{
+            inputUser.classList.remove("invalid");
+        }
+        if(inputPass.value==='') {
+            camposCompletos = false;
+            inputPass.classList.add("invalid");
+        }else{
+            inputPass.classList.remove("invalid");
+        }
+
+        if(camposCompletos) {
+            window.location = "inicio.html";
+        }
+        
+    })
 });
